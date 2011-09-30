7214226230f06f086613ac71dce02788173ef52b
Nathan Dauber <Nathan.Dauber@gmail.com>
2011-09-29 20:39:57 -0500

    clean up

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,9 +2,9 @@ var http = require('http');
 http.createServer(function (req, response) {
   var tweets = {items:[
 	    {latitude:-34.1, longitude:152,type:"height"},
-	    {latitude:-34.25, longitude:152.25,type:"height"},
-	    {latitude:-34.5, longitude:152.5,type:"height"},
-	    {latitude:-34.75, longitude:152.75,type:"height"}
+	    {latitude:-34.25, longitude:152.25,type:"heat"},
+	    {latitude:-34.5, longitude:152.5,type:"light"},
+	    {latitude:-34.75, longitude:152.75,type:"pressure"}
     ]};  
   var body = JSON.stringify(tweets)
   response.writeHead(200, {
