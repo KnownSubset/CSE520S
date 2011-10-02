326451e016b7dc4e8b8c2e821ee22d8e5e1e72e1
Nathan Dauber <Nathan.Dauber@gmail.com>
2011-10-01 19:25:30 -0500

    the webpage can now ask for a specific type of statistics, and will show and hide all of them.  Still need an auto update

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,17 +1,21 @@
 var http = require('http');
+var url = require("url");
+
 http.createServer(function (req, response) {
+  
+  var params = url.parse(req.url).query;
+  var type = params.split('=')[1];
   var tweets = {items:[
-	    {latitude:-34.1, longitude:152,type:"height"},
-	    {latitude:-34.25, longitude:152.25,type:"heat"},
-	    {latitude:-34.5, longitude:152.5,type:"light"},
-	    {latitude:-34.75, longitude:152.75,type:"pressure"}
+	    {latitude:-34.1, longitude:152,type:type},
+	    {latitude:-34.25, longitude:152.25,type:type},
+	    {latitude:-34.5, longitude:152.5,type:type},
+	    {latitude:-34.75, longitude:152.75,type:type}
     ]};  
-  var body = JSON.stringify(tweets)
-  response.writeHead(200, {
-    
-  'Content-Type': 'text/json',
-  'Access-Control-Allow-Origin': '*',
- 'Content-Length': body.length});
+  var body = JSON.stringify(tweets);
+  response.writeHead(200, {	    
+	  'Content-Type': 'text/json',
+	  'Access-Control-Allow-Origin': '*',
+	 'Content-Length': body.length});
   response.write(body);
   response.end();
 }).listen(1337, "127.0.0.1");
